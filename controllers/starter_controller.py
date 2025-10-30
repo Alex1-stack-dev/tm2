76004fb2d011458a54e1e9da65345d620cada4f7
Alex1-stack-dev <alexander_m113@outlook.com>
2025-10-30 12:24:47 -0700

    Create starter_controller.py

---
+++ controllers/starter_controller.py
@@ -0,0 +1,8 @@
+from utils.logger import log_error
+class StarterController:
+    def listen_for_start(self):
+        try:
+            # Implement actual USB/relay/serial listening here
+            pass  # placeholder
+        except Exception as e:
+            log_error(e)
