fd237ccea2f53717b57a5adbd6020b04a455f431
Alex1-stack-dev <alexander_m113@outlook.com>
2025-10-30 12:23:15 -0700

    Create hytek_exporter.py

---
+++ reports/exporters/hytek_exporter.py
@@ -0,0 +1,5 @@
+# Use pyhytek or implement based on .hy3/.cl2 public specs.
+def export_hytek(fname):
+    with open(fname, 'w') as f:
+        f.write(";; HyTek Export Template\n")
+        # Add proper spec implementation
